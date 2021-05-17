f69f899132f5b4fbe0619aa95604d7aca39fc41c
phcalpis <84302031+phcalpis@users.noreply.github.com>
2021-05-17 19:10:20 +0800

    Exercises 1.1-1.5

---
+++ courseinfo/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+
+const Total = ({ parts }) => {
+  let sum = parts[0].exercises + parts[1].exercises + parts[2].exercises;
+  return <p>Number of exercises {sum}</p>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <>
+    <Part part={parts[0]} />
+    <Part part={parts[1]} />
+    <Part part={parts[2]} />
+  </>
+);
+
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default App;
